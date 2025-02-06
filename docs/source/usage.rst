054b174a8552685978d42d8389f6cc0dca59c7ce
kxu03 <xukyle16@gmail.com>
2025-02-05 19:27:14 -0500

    update for package and installation

--- docs/source/usage.rst
+++ docs/source/usage.rst
@@ -10,7 +10,7 @@ To use SyNG-BTS, first install it using pip:
 
 .. code-block:: console
 
-   (.venv) $ pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ syng-bts==2.0
+   (.venv) $ pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ syng-bts==2.1
 
 
 Using Methods
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 import os
+
 lib_folder = os.path.dirname(os.path.realpath(__file__))
 requirement_path = f"{lib_folder}/docs/requirements.txt"
-install_requires = [] # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+install_requires = []  # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
 if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
@@ -12,18 +13,20 @@ print(install_requires)
 
 setup(
     name="syng_bts",
-    version='2.0',
+    version="2.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'syng_bts.RealData': ['SKCMPositive_4.csv'],
-        'syng_bts.Case.BRCASubtype': ['BRCASubtypeSel.csv'],
-        'syng_bts.Case.BRCASubtype': ['BRCASubtypeSel_test.csv'],
-        'syng_bts.Case.BRCASubtype': ['BRCASubtypeSel_train_epoch285_CVAE1-20_generated.csv'],
-        'syng_bts.Case.BRCASubtype': ['BRCASubtypeSel_train.csv'],
-        'syng_bts.Transfer': ['BRCA.csv'],
-        'syng_bts.Transfer': ['PRAD.csv'],
+        "syng_bts.RealData": ["SKCMPositive_4.csv"],
+        "syng_bts.Case.BRCASubtype": ["BRCASubtypeSel.csv"],
+        "syng_bts.Case.BRCASubtype": ["BRCASubtypeSel_test.csv"],
+        "syng_bts.Case.BRCASubtype": [
+            "BRCASubtypeSel_train_epoch285_CVAE1-20_generated.csv"
+        ],
+        "syng_bts.Case.BRCASubtype": ["BRCASubtypeSel_train.csv"],
+        "syng_bts.Transfer": ["BRCA.csv"],
+        "syng_bts.Transfer": ["PRAD.csv"],
     },
-    install_requires = install_requires,
-    python_requires='>=3.6',
+    install_requires=install_requires,
+    python_requires=">=3.6",
 )
